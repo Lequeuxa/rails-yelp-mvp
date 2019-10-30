84e6d06e0ce2eea86f5868787b9fcf9973e9caf1
Antoine Lequeux <lequeuxa@gmail.com>
2019-10-30 16:17:56 +0100

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,6 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian']
   has_many :reviews, dependent: :destroy
-  validates :name, :address, :category, presence: :true
-  validates :category, inclusion: {
-    in: %w[chinese italian japanese french belgian]
-  }
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: CATEGORY }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
+  RATING = [0, 1, 2, 3, 4, 5]
   belongs_to :restaurant
-  validates :content, :rating, presence: :true
-  validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: RATING }, numericality: { only_integer: true }
 end
